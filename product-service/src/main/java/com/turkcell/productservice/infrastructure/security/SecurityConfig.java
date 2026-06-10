3b6a24c76140c5e469fddbd39e2e8a7ec238f9f3
halitklyc <halitenes.kalayci@gmail.com>
2026-06-10 10:43:47 +0300

    add resource server pt1

---
+++ product-service/src/main/java/com/turkcell/productservice/infrastructure/security/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.turkcell.productservice.infrastructure.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http    // Yetkinlendirme kuralları (URL BAZLI) (Resource-Level)
+                .authorizeHttpRequests(
+                  auth -> auth
+                          .requestMatchers("/actuator/**","/swagger-ui/**","/v3/api-docs/**","/h2-console/**").permitAll()
+                          .requestMatchers(HttpMethod.GET, "/api/v1/products/**").authenticated()
+                          .requestMatchers(HttpMethod.POST, "/api/v1/products/**").hasRole("ADMIN")
+                          .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 ->
+                        oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))
+                )
+                // REST API Statlessdır -> CSRF ve session kapalı
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                // h2 console -> iframe ile çalıştığı için
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
+        return http.build();
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
+        return converter;
+    }
+}
